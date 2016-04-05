357087fe05ae8a5668b2d401acd518663ac608a4
Alex <awb7422@uncw.edu>
2016-04-04 21:04:51 -0400

    added basic elements to search results page, needs refining

--- Andriod/app/src/main/java/carenthusiasts/andriod/SearchResultsActivity.java
+++ Andriod/app/src/main/java/carenthusiasts/andriod/SearchResultsActivity.java
@@ -1,13 +1,27 @@
 package carenthusiasts.andriod;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
-public class SearchResultsActivity extends AppCompatActivity {
+public class SearchResultsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+        loadCars(savedInstanceState);
+    }
+    private void loadCars(Bundle savedInstanceState){
+        if (savedInstanceState == null) {
+            Fragment newFragment = new CarsFragment();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.add(R.id.frame1, newFragment).commit();
+        }
     }
 }
