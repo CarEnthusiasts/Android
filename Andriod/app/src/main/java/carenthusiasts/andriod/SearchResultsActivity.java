60d0a914f4a2f27fd6c12ba9bc82adcd1642b544
Alex <awb7422@uncw.edu>
2016-04-12 17:25:25 -0400

    made changes to search results prior to implementing the connector to database... also added project example to insert into database, and an example to populate listview from database using json for both.

--- Andriod/app/src/main/java/carenthusiasts/andriod/SearchResultsActivity.java
+++ Andriod/app/src/main/java/carenthusiasts/andriod/SearchResultsActivity.java
@@ -3,9 +3,11 @@ package carenthusiasts.andriod;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
@@ -19,9 +21,8 @@ public class SearchResultsActivity extends Activity {
     }
     private void loadCars(Bundle savedInstanceState){
         if (savedInstanceState == null) {
-            Fragment newFragment = new CarsFragment();
-            FragmentTransaction ft = getFragmentManager().beginTransaction();
-            ft.add(R.id.frame1, newFragment).commit();
+            FrameLayout newCar = new fragment_cars()
+
         }
     }
 }
